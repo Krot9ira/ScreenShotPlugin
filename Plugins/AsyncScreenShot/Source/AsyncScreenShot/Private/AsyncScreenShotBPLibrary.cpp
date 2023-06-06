7f3f1e7f6306d522894ed57f62719d570b08b6b5
Krot9ira <90332757+Krot9ira@users.noreply.github.com>
2023-06-06 07:36:25 +0300

    Fully working wndow screenmaker

--- Plugins/AsyncScreenShot/Source/AsyncScreenShot/Private/AsyncScreenShotBPLibrary.cpp
+++ Plugins/AsyncScreenShot/Source/AsyncScreenShot/Private/AsyncScreenShotBPLibrary.cpp
@@ -155,6 +155,175 @@ bool SaveToMemory(HBITMAP* hbitmap, std::vector<BYTE>& data, std::string dataFor
     return true;
 }
 
+int CaptureAnImage(HWND hWnd, string path)
+{
+    HDC hdcScreen;
+    HDC hdcWindow;
+    HDC hdcMemDC = NULL;
+    HBITMAP hbmScreen = NULL;
+    BITMAP bmpScreen;
+    DWORD dwBytesWritten = 0;
+    DWORD dwSizeofDIB = 0;
+    HANDLE hFile = NULL;
+    char* lpbitmap = NULL;
+    HANDLE hDIB = NULL;
+    DWORD dwBmpSize = 0;
+
+    // Retrieve the handle to a display device context for the client 
+    // area of the window. 
+    hdcScreen = GetDC(0);
+    hdcWindow = GetDC(hWnd);
+
+    // Create a compatible DC, which is used in a BitBlt from the window DC.
+    hdcMemDC = CreateCompatibleDC(hdcWindow);
+
+    if (!hdcMemDC)
+    {
+        MessageBox(hWnd, L"CreateCompatibleDC has failed", L"Failed", MB_OK);
+        DeleteObject(hbmScreen);
+        DeleteObject(hdcMemDC);
+        ReleaseDC(NULL, hdcScreen);
+        ReleaseDC(hWnd, hdcWindow);
+
+        return 0;
+    }
+
+    // Get the client area for size calculation.
+    RECT rcClient;
+    GetClientRect(hWnd, &rcClient);
+    WINDOWPLACEMENT WindowPosition = {};
+    WindowPosition.length = sizeof(WINDOWPLACEMENT);
+    GetWindowPlacement(hWnd, &WindowPosition);
+    // This is the best stretch mode.
+    SetStretchBltMode(hdcWindow, COLORONCOLOR);
+    
+    // The source DC is the entire screen, and the destination DC is the current window (HWND).
+    if (!StretchBlt(hdcWindow,
+        0, 0,
+        rcClient.right, rcClient.bottom,
+        hdcScreen,
+        WindowPosition.rcNormalPosition.left, WindowPosition.rcNormalPosition.top,
+        rcClient.right, rcClient.bottom,
+        SRCCOPY))
+    {
+        MessageBox(hWnd, L"StretchBlt has failed", L"Failed", MB_OK);
+        DeleteObject(hbmScreen);
+        DeleteObject(hdcMemDC);
+        ReleaseDC(NULL, hdcScreen);
+        ReleaseDC(hWnd, hdcWindow);
+
+        return 0;
+    }
+
+    // Create a compatible bitmap from the Window DC.
+    hbmScreen = CreateCompatibleBitmap(hdcWindow, rcClient.right - rcClient.left, rcClient.bottom - rcClient.top);
+
+    if (!hbmScreen)
+    {
+        MessageBox(hWnd, L"CreateCompatibleBitmap Failed", L"Failed", MB_OK);
+        DeleteObject(hbmScreen);
+        DeleteObject(hdcMemDC);
+        ReleaseDC(NULL, hdcScreen);
+        ReleaseDC(hWnd, hdcWindow);
+
+        return 0;
+    }
+
+    // Select the compatible bitmap into the compatible memory DC.
+    SelectObject(hdcMemDC, hbmScreen);
+
+    // Bit block transfer into our compatible memory DC.
+    if (!BitBlt(hdcMemDC,
+        0, 0,
+        rcClient.right - rcClient.left, rcClient.bottom - rcClient.top,
+        hdcWindow,
+        0, 0,
+        SRCCOPY))
+    {
+        MessageBox(hWnd, L"BitBlt has failed", L"Failed", MB_OK);
+        DeleteObject(hbmScreen);
+        DeleteObject(hdcMemDC);
+        ReleaseDC(NULL, hdcScreen);
+        ReleaseDC(hWnd, hdcWindow);
+
+        return 0;
+    }
+
+    // Get the BITMAP from the HBITMAP.
+    GetObject(hbmScreen, sizeof(BITMAP), &bmpScreen);
+
+    BITMAPFILEHEADER   bmfHeader;
+    BITMAPINFOHEADER   bi;
+
+    bi.biSize = sizeof(BITMAPINFOHEADER);
+    bi.biWidth = bmpScreen.bmWidth;
+    bi.biHeight = bmpScreen.bmHeight;
+    bi.biPlanes = 1;
+    bi.biBitCount = 32;
+    bi.biCompression = BI_RGB;
+    bi.biSizeImage = 0;
+    bi.biXPelsPerMeter = 0;
+    bi.biYPelsPerMeter = 0;
+    bi.biClrUsed = 0;
+    bi.biClrImportant = 0;
+
+    dwBmpSize = ((bmpScreen.bmWidth * bi.biBitCount + 31) / 32) * 4 * bmpScreen.bmHeight;
+
+    // Starting with 32-bit Windows, GlobalAlloc and LocalAlloc are implemented as wrapper functions that 
+    // call HeapAlloc using a handle to the process's default heap. Therefore, GlobalAlloc and LocalAlloc 
+    // have greater overhead than HeapAlloc.
+    hDIB = GlobalAlloc(GHND, dwBmpSize);
+    lpbitmap = (char*)GlobalLock(hDIB);
+
+    // Gets the "bits" from the bitmap, and copies them into a buffer 
+    // that's pointed to by lpbitmap.
+    GetDIBits(hdcWindow, hbmScreen, 0,
+        (UINT)bmpScreen.bmHeight,
+        lpbitmap,
+        (BITMAPINFO*)&bi, DIB_RGB_COLORS);
+    std::wstring stemp = std::wstring(path.begin(), path.end());
+    LPCWSTR sw = stemp.c_str();
+    // A file is created, this is where we will save the screen capture.
+    hFile = CreateFile(sw,
+        GENERIC_WRITE,
+        0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL, NULL);
+
+    // Add the size of the headers to the size of the bitmap to get the total file size.
+    dwSizeofDIB = dwBmpSize + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+
+    // Offset to where the actual bitmap bits start.
+    bmfHeader.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) + (DWORD)sizeof(BITMAPINFOHEADER);
+
+    // Size of the file.
+    bmfHeader.bfSize = dwSizeofDIB;
+
+    // bfType must always be BM for Bitmaps.
+    bmfHeader.bfType = 0x4D42; // BM.
+
+    WriteFile(hFile, (LPSTR)&bmfHeader, sizeof(BITMAPFILEHEADER), &dwBytesWritten, NULL);
+    WriteFile(hFile, (LPSTR)&bi, sizeof(BITMAPINFOHEADER), &dwBytesWritten, NULL);
+    WriteFile(hFile, (LPSTR)lpbitmap, dwBmpSize, &dwBytesWritten, NULL);
+
+    // Unlock and Free the DIB from the heap.
+    GlobalUnlock(hDIB);
+    GlobalFree(hDIB);
+
+    // Close the handle for the file that was created.
+    CloseHandle(hFile);
+
+    // Clean up.
+
+    DeleteObject(hbmScreen);
+    DeleteObject(hdcMemDC);
+    ReleaseDC(NULL, hdcScreen);
+    ReleaseDC(hWnd, hdcWindow);
+
+    return 0;
+}
+
 
 
 
@@ -172,22 +341,16 @@ void UAsyncScreenShotBPLibrary::SaveGameScreen(FString PathToSave, FString Name)
         ULONG_PTR GDIplusToken;
         GdiplusStartup(&GDIplusToken, &GDIplusStartupInput, NULL);
 
-        FString FullPath = PathToSave + FString("\\") + Name + FString(".");
+       
+
+        FString FullPath = PathToSave + FString("\\") + Name + FString(".bmp");
 
         // get the bitmap handle to the bitmap screenshot
         HWND hWnd = static_cast<HWND>(GEngine->GameViewport->GetWindow()->GetNativeWindow()->GetOSWindowHandle());
-        HBITMAP hBmp = GdiPlusScreenCapture(hWnd);
 
+        CaptureAnImage(hWnd, std::string(TCHAR_TO_UTF8(*FullPath)));
         // save as png to memory 
-        std::vector<BYTE> data;
-        std::string dataFormat = "bmp";
-
-        if (SaveToMemory(&hBmp, data, dataFormat))
-        {
-            // save from memory to file
-            std::ofstream fout(std::string(TCHAR_TO_UTF8(*FullPath)) + dataFormat, std::ios::binary);
-            fout.write((char*)data.data(), data.size());
-        }
+       
 
         GdiplusShutdown(GDIplusToken);
 
