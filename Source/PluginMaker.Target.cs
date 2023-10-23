961efb2d065af0e2b54840ed648dbee2ec173949
Krot9ira <90332757+Krot9ira@users.noreply.github.com>
2023-10-23 17:19:23 +0300

    5.3 update

--- Source/PluginMaker.Target.cs
+++ Source/PluginMaker.Target.cs
@@ -8,8 +8,8 @@ public class PluginMakerTarget : TargetRules
 	public PluginMakerTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.V4;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("PluginMaker");
 	}
 }
