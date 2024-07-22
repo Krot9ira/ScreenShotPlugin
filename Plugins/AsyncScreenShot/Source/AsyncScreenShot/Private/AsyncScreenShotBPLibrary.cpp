9a99eff0f5caf22058bd6a0072fe94fcba7acd0d
Krot9ira <90332757+Krot9ira@users.noreply.github.com>
2024-07-22 11:55:27 +0300

    Rewrite save render target to new method
    Previos one dont have any spike, but crash in packaged builds.
    New version is working not so good in general but at least work in packaged build

--- Plugins/AsyncScreenShot/Source/AsyncScreenShot/Private/AsyncScreenShotBPLibrary.cpp
+++ Plugins/AsyncScreenShot/Source/AsyncScreenShot/Private/AsyncScreenShotBPLibrary.cpp
@@ -17,7 +17,7 @@
 #include "Async/Async.h"
 
 UAsyncScreenShotBPLibrary::UAsyncScreenShotBPLibrary(const FObjectInitializer& ObjectInitializer)
-: Super(ObjectInitializer)
+    : Super(ObjectInitializer)
 {
 
 }
@@ -55,7 +55,7 @@ using namespace std;
 void Bgra2Rgb(const unsigned char* src, int w, int h, int d, unsigned char* dst)
 {
     unsigned char* pTempDst = dst;
-    for (int i = abs(h)-1; i >= 0; i--)
+    for (int i = abs(h) - 1; i >= 0; i--)
     {
         const unsigned char* pTempSrc = nullptr;
         if (h > 0)
@@ -119,29 +119,29 @@ int CaptureAnImage(HWND hWnd, string path, EImageFormat ImageFormat, int quality
     // This is the best stretch mode.
     SetStretchBltMode(hdcWindow, COLORONCOLOR);
     // TakeAllScreen
-        if (!StretchBlt(hdcWindow,
-            0, 0,
-            rcClient.right, rcClient.bottom,
-            hdcScreen,
-            rcWindow.left, rcWindow.top,
-            rcClient.right, rcClient.bottom,
-            SRCCOPY))
-        {
-            MessageBox(hWnd, L"StretchBlt has failed", L"Failed", MB_OK);
-            DeleteObject(hbmScreen);
-            DeleteObject(hdcMemDC);
-            ReleaseDC(NULL, hdcScreen);
-            ReleaseDC(hWnd, hdcWindow);
+    if (!StretchBlt(hdcWindow,
+        0, 0,
+        rcClient.right, rcClient.bottom,
+        hdcScreen,
+        rcWindow.left, rcWindow.top,
+        rcClient.right, rcClient.bottom,
+        SRCCOPY))
+    {
+        MessageBox(hWnd, L"StretchBlt has failed", L"Failed", MB_OK);
+        DeleteObject(hbmScreen);
+        DeleteObject(hdcMemDC);
+        ReleaseDC(NULL, hdcScreen);
+        ReleaseDC(hWnd, hdcWindow);
 
-            return 0;
-        }
+        return 0;
+    }
 
-    
-    
+
+
 
     // Create a compatible bitmap from the Window DC.
-        long x = rcClient.right - rcClient.left;
-        long y = rcClient.bottom - rcClient.top;
+    long x = rcClient.right - rcClient.left;
+    long y = rcClient.bottom - rcClient.top;
     hbmScreen = CreateCompatibleBitmap(hdcWindow, x, y);
 
     if (!hbmScreen)
@@ -219,7 +219,7 @@ int CaptureAnImage(HWND hWnd, string path, EImageFormat ImageFormat, int quality
             CREATE_ALWAYS,
             FILE_ATTRIBUTE_NORMAL, NULL);
     }
-    
+
 
     // Add the size of the headers to the size of the bitmap to get the total file size.
     dwSizeofDIB = dwBmpSize + sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
@@ -242,14 +242,14 @@ int CaptureAnImage(HWND hWnd, string path, EImageFormat ImageFormat, int quality
         break;
     case EImageFormat::jpg:
         rgb = new unsigned char[x * abs(y) * 32 / 8];
-        Bgra2Rgb((unsigned char* )lpbitmap, x, y, 32 / 8, rgb);
-       
-       stbi_write_jpg(path.data(), x, y, 3, rgb, quality);
+        Bgra2Rgb((unsigned char*)lpbitmap, x, y, 32 / 8, rgb);
+
+        stbi_write_jpg(path.data(), x, y, 3, rgb, quality);
         break;
     default:
         break;
     }
-   
+
 
     // Unlock and Free the DIB from the heap.
     GlobalUnlock(hDIB);
@@ -275,13 +275,13 @@ int CaptureAnImage(HWND hWnd, string path, EImageFormat ImageFormat, int quality
 
 void UAsyncScreenShotBPLibrary::SaveGameScreen(FString PathToSave, FString Name, EImageFormat ImageFormat, int Quality)
 {
-   
+
     if (Name == "")
     {
         Name = "Blank";
     }
-    
-    
+
+
     AsyncTask(ENamedThreads::AnyBackgroundThreadNormalTask, [PathToSave, Name, ImageFormat, Quality] {
         GdiplusStartupInput GDIplusStartupInput;
         ULONG_PTR GDIplusToken;
@@ -298,7 +298,7 @@ void UAsyncScreenShotBPLibrary::SaveGameScreen(FString PathToSave, FString Name,
         default:
             break;
         }
-        FString FullPath = PathToSave  + Name + FormatEnd;
+        FString FullPath = PathToSave + Name + FormatEnd;
         string FolderPath = std::string(TCHAR_TO_UTF8(*PathToSave));
         FolderPath.pop_back();
         std::wstring stemp = std::wstring(FolderPath.begin(), FolderPath.end());
@@ -311,25 +311,25 @@ void UAsyncScreenShotBPLibrary::SaveGameScreen(FString PathToSave, FString Name,
         {
             filesystem::create_directories(FolderPath);
         }
-        
+
         CaptureAnImage(hWnd, std::string(TCHAR_TO_UTF8(*FullPath)), ImageFormat, Quality);
         // save as png to memory 
-       
+
 
         GdiplusShutdown(GDIplusToken);
 
         });
 
-    
+
     /* Native c++ multihread
     std::thread my_thread([PathToSave, Name] {
-        
+
 
         GdiplusStartupInput GDIplusStartupInput;
         ULONG_PTR GDIplusToken;
         GdiplusStartup(&GDIplusToken, &GDIplusStartupInput, NULL);
 
-       
+
 
         FString FullPath = PathToSave + FString("\\") + Name + FString(".bmp");
 
@@ -337,83 +337,156 @@ void UAsyncScreenShotBPLibrary::SaveGameScreen(FString PathToSave, FString Name,
         HWND hWnd = static_cast<HWND>(GEngine->GameViewport->GetWindow()->GetNativeWindow()->GetOSWindowHandle());
 
         CaptureAnImage(hWnd, std::string(TCHAR_TO_UTF8(*FullPath)));
-        // save as png to memory 
-       
+        // save as png to memory
+
 
         GdiplusShutdown(GDIplusToken);
 
         });
-   
+
     my_thread.detach();
     */
 }
-
-void UAsyncScreenShotBPLibrary::SaveRenderTarget(UTextureRenderTarget2D* RenderTarget, FString PathToSave, FString Name)
+void PollRTRead(FRHICommandListImmediate& RHICmdList, TSharedPtr<FAsyncReadEntireRTData, ESPMode::ThreadSafe> ReadData, TWeakObjectPtr<UAsyncScreenshotRTAction> ReadAction, bool bFlushRHI)
 {
-    ETextureSourceFormat OutTextureSourceFormat;
-    EPixelFormat OutPixelFormat;
-    FText* OutErrorMessage = nullptr;
-    bool Can = RenderTarget->CanConvertToTexture(OutTextureSourceFormat, OutPixelFormat, OutErrorMessage);
-    if (!RenderTarget || OutTextureSourceFormat != ETextureSourceFormat::TSF_RGBA16F)
+    SCOPED_NAMED_EVENT_TEXT("AsyncScreenshot::AsyncReadRT::PollRTRead", FColor::Magenta);
+
+    check(IsInRenderingThread());
+
+
+    // If we didn't flush the RHI then make sure the previous rendering commands got done
+    if (!bFlushRHI)
     {
-        return;
-    }
-    AsyncTask(ENamedThreads::AnyBackgroundThreadNormalTask, [RenderTarget, PathToSave, Name]() {
-    int32 InSizeX = RenderTarget->SizeX;
-    int32 InSizeY = RenderTarget->SizeY;
-
-    struct FCopyBufferData {
-        TPromise<void> Promise;
-        TArray<FFloat16Color> StructDestBuffer;
-    };
-
-    TArray<FFloat16Color> PixelArray;
-    int32 sizeOfImage = InSizeX * InSizeY;
-    PixelArray.SetNum(sizeOfImage);
-
-    using FCommandDataPtr = TSharedPtr<FCopyBufferData, ESPMode::ThreadSafe>;
-    FCommandDataPtr CommandData = MakeShared<FCopyBufferData, ESPMode::ThreadSafe>();
-    CommandData->StructDestBuffer.SetNum(sizeOfImage);
-
-    auto Future = CommandData->Promise.GetFuture();
-
-    //CopyTextureToArray(InTexture, PixelArray);
-    ENQUEUE_RENDER_COMMAND(CopyTexture2DToArray)([Data = CommandData, RenderTarget, bFlush = false](FRHICommandListImmediate& RHICmdList)
+        // Return if we haven't finished the texture commands
+        if (!ReadData->TextureFence.IsValid() || !ReadData->TextureFence->Poll() || ReadData->StartReading)
         {
-    if (RenderTarget && RenderTarget->GetResource() && RenderTarget->GetResource()->TextureRHI.IsValid() && RenderTarget->GetResource()->TextureRHI->GetTexture2D()->IsValid()) {
-        UE_LOG(LogTemp, Warning, TEXT("Valid, Starting Read"));
-        const uint32 NumBytes = CalculateImageBytes(RenderTarget->SizeX, RenderTarget->SizeY, 0, RenderTarget->GetFormat());
-
-            
-         uint32 DestStride = 0;
-         FFloat16Color* DestBuffer = static_cast<FFloat16Color*>(RHILockTexture2D(RenderTarget->GetResource()->TextureRHI->GetTexture2D(), 0, EResourceLockMode::RLM_ReadOnly, DestStride, false));
-
-         FMemory::Memcpy(Data->StructDestBuffer.GetData(), DestBuffer, NumBytes);
-
-         RHIUnlockTexture2D(RenderTarget->GetResource()->TextureRHI->GetTexture2D(), 0, false);
-         Data->Promise.SetValue();
+            return;
+        }
     }
+    ReadData->FinishedRead = false;
+    SCOPED_NAMED_EVENT_TEXT("AsyncScreenshot::AsyncReadRT::MapTexture", FColor::Magenta);
+    void* OutputBuffer = NULL;
+    int32 RowPitchInPixels, Height;
+
+    if (bFlushRHI)
+    {
+        // This flushes the command list
+        RHICmdList.MapStagingSurface(ReadData->Texture, ReadData->TextureFence, OutputBuffer, RowPitchInPixels, Height);
+    }
+    else
+    {
+#if ENGINE_MAJOR_VERSION >= 5 && ENGINE_MINOR_VERSION >= 2
+        GDynamicRHI->RHIMapStagingSurface_RenderThread(RHICmdList, ReadData->Texture, INDEX_NONE, ReadData->TextureFence, OutputBuffer, RowPitchInPixels, Height);
+#else
+        GDynamicRHI->RHIMapStagingSurface_RenderThread(RHICmdList, ReadData->Texture, ReadData->TextureFence, OutputBuffer, RowPitchInPixels, Height);
+#endif
+    }
+    ReadData->StartReading = true;
+    const int32 Width = ReadData->Texture->GetSizeX();
+    check(RowPitchInPixels >= Width);
+    check(Height == ReadData->Texture->GetSizeY());
+    const int32 SrcPitch = RowPitchInPixels * GPixelFormats[ReadData->Texture->GetFormat()].BlockBytes;
+    const uint32 NumBytes = CalculateImageBytes(Width, Height, 0, ReadData->Texture->GetFormat());
+    ReadData->PixelColors.Empty(Width * Height);
+    ReadData->PixelColors.SetNum(Width * Height);
+    const EPixelFormat Format = ReadData->Texture->GetFormat();
+    if (Format == EPixelFormat::PF_B8G8R8A8)
+    {
+        FMemory::Memmove(ReadData->PixelColors.GetData(), OutputBuffer, NumBytes);
+    }
+    /*
+    for (int32 YIndex = 0; YIndex < Height; YIndex++)
+    {
+        for (int32 X = 0; X < Width; ++X)
+        {
+            const int32 PixelOffset = X + (YIndex * RowPitchInPixels);
+
+            FLinearColor& OutColor = ReadData->PixelColors.AddDefaulted_GetRef();
+            switch (Format)
+            {
+            case EPixelFormat::PF_FloatRGBA:
+            {
+                FFloat16Color* OutputColor = reinterpret_cast<FFloat16Color*>(OutputBuffer) + PixelOffset;
+                OutColor.R = OutputColor->R;
+                OutColor.G = OutputColor->G;
+                OutColor.B = OutputColor->B;
+                OutColor.A = OutputColor->A;
+                break;
+            }
+            case EPixelFormat::PF_B8G8R8A8:
+            {
+                FColor* OutputColor = reinterpret_cast<FColor*>(OutputBuffer) + PixelOffset;
+                OutColor.R = OutputColor->R;
+                OutColor.G = OutputColor->G;
+                OutColor.B = OutputColor->B;
+                OutColor.A = OutputColor->A;
+                OutColor /= 255.f;
+                break;
+            }
+            default:
+                UE_LOG(LogTemp, Warning, TEXT("AsyncScreenshot: Unsupported RT format! Format: %d"), static_cast<int32>(Format)); // Unsupported, add a new switch statement.
+            }
+        }
+    }
+    */
+    RHICmdList.UnmapStagingSurface(ReadData->Texture);
+    ReadData->FinishedRead = true;
+}
+UAsyncScreenshotRTAction* UAsyncScreenshotRTAction::SaveRenderTarget(UObject* WorldContextObject, UTextureRenderTarget2D* RenderTarget, FString PathToSave, FString Name, bool bFlushRHI)
+{
+    UAsyncScreenshotRTAction* BlueprintNode = NewObject<UAsyncScreenshotRTAction>();
+    BlueprintNode->WorldContextObject = WorldContextObject;
+    BlueprintNode->RT = RenderTarget;
+    BlueprintNode->bFlushRHI = bFlushRHI;
+    BlueprintNode->ReadRTData = MakeShared<FAsyncReadEntireRTData, ESPMode::ThreadSafe>();
+    BlueprintNode->ReadRTData->FinishedRead = false;
+    BlueprintNode->SavedPathToSave = PathToSave;
+    BlueprintNode->SavedName = Name;
+    return BlueprintNode;
+
+
+
+}
+
+void WritePixelsToFile(FTexture2DRHIRef RenderTarget, FString PathToSave, FString Name, TArray<FColor>& PixelToCopy, TWeakObjectPtr<UAsyncScreenshotRTAction> Action)
+{
+
+    AsyncTask(ENamedThreads::AnyBackgroundThreadNormalTask, [Action, RenderTarget, PathToSave, Name, &PixelToCopy]() {
+        int32 InSizeX = RenderTarget->GetSizeX();
+        int32 InSizeY = RenderTarget->GetSizeY();
+        SCOPED_NAMED_EVENT_TEXT("AsyncScreenshot::AsyncReadRT::WriteImage", FColor::Magenta);
+
+
+        TArray<FColor> PixelArray;
+        int32 sizeOfImage = InSizeX * InSizeY;
+        PixelArray.SetNum(sizeOfImage);
+
+
+        PixelArray = std::move(PixelToCopy);
+        int32 size = PixelArray.Num();
+
+        FString FullPath = PathToSave + Name + FString(".png");
+        string FolderPath = std::string(TCHAR_TO_UTF8(*FullPath));
+
+        std::vector<uint8> data;
+        FString pixString;
+        for (int i = 0; i < size; i++) {
+            data.push_back(PixelArray[i].R);
+            data.push_back(PixelArray[i].G);
+            data.push_back(PixelArray[i].B);
+            data.push_back(255);
+        }
+        stbi_write_png(FolderPath.data(), InSizeX, InSizeY, 4, static_cast<void*>(data.data()), 4 * InSizeX);
+
+        AsyncTask(ENamedThreads::GameThread, [Action]() {
+            if (Action.IsValid())
+            {
+                Action->OnSaveRenderTarget.Broadcast();
+                Action->SetReadyToDestroy();
+            }
             });
 
-    
-
-    Future.Get();
-    PixelArray = std::move(CommandData->StructDestBuffer);
-    int32 size = PixelArray.Num();
-
-    FString FullPath = PathToSave + Name + FString(".png");
-    string FolderPath = std::string(TCHAR_TO_UTF8(*FullPath));
-
-    std::vector<uint8> data;
-    FString pixString;
-    for (int i = 0; i < size; i++) {
-        data.push_back(PixelArray[i].GetFloats().ToFColor(true).R);
-        data.push_back(PixelArray[i].GetFloats().ToFColor(true).G);
-        data.push_back(PixelArray[i].GetFloats().ToFColor(true).B);
-        data.push_back(255);
-    }
-    stbi_write_png(FolderPath.data(), RenderTarget->SizeX, RenderTarget->SizeY, 4, static_cast<void*>(data.data()), 4 * RenderTarget->SizeX); });
-    
+        });
 }
 
 #endif
@@ -421,8 +494,97 @@ FString UAsyncScreenShotBPLibrary::GetScreenshotSavePath()
 {
     return FPaths::ConvertRelativePathToFull(FPaths::ScreenShotDir());
 }
+void UAsyncScreenshotRTAction::OnNextFrame()
+{
+    check(IsInGameThread());
+    check(ReadRTData.IsValid());
+
+    if (ReadRTData->FinishedRead)
+    {
+        WritePixelsToFile(ReadRTData->Texture, SavedPathToSave, SavedName, ReadRTData->PixelColors, this);
+        return;
+    }
+    else
+    {
+        ENQUEUE_RENDER_COMMAND(FReadRTAsync)([WeakThis = TWeakObjectPtr<UAsyncScreenshotRTAction>(this), ReadRTData = ReadRTData](FRHICommandListImmediate& RHICmdList)
+            {
+                PollRTRead(RHICmdList, ReadRTData, WeakThis, false);
+            });
+
+
+
+        WorldContextObject->GetWorld()->GetTimerManager().SetTimerForNextTick(this, &UAsyncScreenshotRTAction::OnNextFrame);
+    }
+}
 #if !PLATFORM_WINDOWS
 void UAsyncScreenShotBPLibrary::SaveGameScreen(FString PathToSave, FString Name)
 {
 }
 #endif
+
+void UAsyncScreenshotRTAction::Activate()
+{
+
+    FTextureRenderTarget2DResource* TextureResource = (FTextureRenderTarget2DResource*)RT->GetResource();
+    check(TextureResource);
+    check(TextureResource->GetRenderTargetTexture());
+
+    StartFrame = GFrameCounter;
+
+    ENQUEUE_RENDER_COMMAND(FCopyRTAsync)([bFlushRHI = bFlushRHI, AsyncReadPtr = TWeakObjectPtr<UAsyncScreenshotRTAction>(this), TextureRHI = TextureResource->GetRenderTargetTexture(), ReadData = ReadRTData](FRHICommandListImmediate& RHICmdList)
+        {
+            check(IsInRenderingThread());
+            check(TextureRHI.IsValid());
+
+            FGPUFenceRHIRef Fence = RHICreateGPUFence(TEXT("AsyncScreenshotRTReadback"));
+
+            SCOPED_NAMED_EVENT_TEXT("AsyncScreenshot::AsyncReadRT", FColor::Magenta);
+
+            FTexture2DRHIRef IORHITextureCPU;
+            {
+                SCOPED_NAMED_EVENT_TEXT("AsyncScreenshot::AsyncReadRT::CreateCopyTexture", FColor::Magenta);
+
+                int32 Width, Height;
+                Width = TextureRHI->GetSizeX();
+                Height = TextureRHI->GetSizeY();
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION > 2
+                FRHITextureCreateDesc TextureDesc = FRHITextureCreateDesc::Create2D(TEXT("AsyncScreenshotRTReadback"), Width, Height, TextureRHI->GetFormat());
+                TextureDesc.AddFlags(ETextureCreateFlags::CPUReadback);
+                TextureDesc.InitialState = ERHIAccess::CopyDest;
+#if ENGINE_MINOR_VERSION > 3
+                IORHITextureCPU = GDynamicRHI->RHICreateTexture(FRHICommandListExecutor::GetImmediateCommandList(), TextureDesc);
+#else // ENGINE_MINOR_VERSION
+                IORHITextureCPU = GDynamicRHI->RHICreateTexture(TextureDesc);
+#endif // ENGINE_MINOR_VERSION
+#else
+                FRHIResourceCreateInfo CreateInfo(TEXT("AsyncRTReadback"));
+                IORHITextureCPU = RHICreateTexture2D(Width, Height, TextureRHI->GetFormat(), 1, 1, TexCreate_CPUReadback, ERHIAccess::CopyDest, CreateInfo);
+#endif
+
+                FRHICopyTextureInfo CopyTextureInfo;
+                CopyTextureInfo.Size = FIntVector(Width, Height, 1);
+                CopyTextureInfo.SourceMipIndex = 0;
+                CopyTextureInfo.DestMipIndex = 0;
+                CopyTextureInfo.SourcePosition = FIntVector(0, 0, 0);
+                CopyTextureInfo.DestPosition = FIntVector(0, 0, 0);
+
+                RHICmdList.Transition(FRHITransitionInfo(TextureRHI, ERHIAccess::Unknown, ERHIAccess::CopySrc));
+                RHICmdList.CopyTexture(TextureRHI, IORHITextureCPU, CopyTextureInfo);
+
+                RHICmdList.Transition(FRHITransitionInfo(IORHITextureCPU, ERHIAccess::CopyDest, ERHIAccess::CopySrc));
+                RHICmdList.WriteGPUFence(Fence);
+            }
+            check(Fence.IsValid());
+
+            ReadData->Texture = IORHITextureCPU;
+            ReadData->TextureFence = Fence;
+
+            // If we flush the RHI then we can just go ahead and read the mapped texture asap
+            if (bFlushRHI)
+            {
+                PollRTRead(RHICmdList, ReadData, AsyncReadPtr, bFlushRHI);
+            }
+        });
+
+    WorldContextObject->GetWorld()->GetTimerManager().SetTimerForNextTick(this, &UAsyncScreenshotRTAction::OnNextFrame);
+}
--- Plugins/AsyncScreenShot/Source/AsyncScreenShot/Public/AsyncScreenShotBPLibrary.h
+++ Plugins/AsyncScreenShot/Source/AsyncScreenShot/Public/AsyncScreenShotBPLibrary.h
@@ -4,9 +4,11 @@
 
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "Engine/TextureRenderTarget2D.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
 #include "AsyncScreenShotBPLibrary.generated.h"
 
-/* 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FAsyncReadEntireRTOutputPin);
+/*
 *	Function library class.
 *	Each function in it is expected to be static and represents blueprint node that can be called in any blueprint.
 *
@@ -16,7 +18,7 @@
 *	DisplayName - full name of the node, shown when you mouse over the node and in the blueprint drop down menu.
 *				Its lets you name the node using characters not allowed in C++ function names.
 *	CompactNodeTitle - the word(s) that appear on the node.
-*	Keywords -	the list of keywords that helps you to find node when you search for it using Blueprint drop-down menu. 
+*	Keywords -	the list of keywords that helps you to find node when you search for it using Blueprint drop-down menu.
 *				Good example is "Print String" node which you can find also by using keyword "log".
 *	Category -	the category your node will be under in the Blueprint drop-down menu.
 *
@@ -30,20 +32,71 @@ enum class EImageFormat : uint8
 	jpg UMETA(DisplayName = "JPG")
 };
 
+
+
+class UTextureRenderTarget2D;
+
+struct FAsyncReadEntireRTData
+{
+	FGPUFenceRHIRef TextureFence;
+	FTexture2DRHIRef Texture;
+	TAtomic<bool> FinishedRead;
+	bool StartReading = false;
+	TArray<FColor> PixelColors;
+
+};
+
 UCLASS()
 class UAsyncScreenShotBPLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_UCLASS_BODY()
-		/*When specifying the path, use / , for example C : / Users / Krot9ira /
-		* Quality is used only for jpg in range from 1 to 100
-		*/
+	/*When specifying the path, use / , for example C : / Users / Krot9ira /
+	* Quality is used only for jpg in range from 1 to 100
+	*/
 	UFUNCTION(BlueprintCallable, meta = (Category = "ScreenshotTaker Functionality"))
-		static void SaveGameScreen(FString PathToSave, FString Name, EImageFormat ImageFormat, int Quality = 80);
-		//TODO Add selecting file extention
-	UFUNCTION(BlueprintCallable, meta = (Category = "ScreenshotTaker Functionality"))
-		static void SaveRenderTarget(UTextureRenderTarget2D* RenderTarget,FString PathToSave, FString Name);
+	static void SaveGameScreen(FString PathToSave, FString Name, EImageFormat ImageFormat, int Quality = 80);
+	//TODO Add selecting file extention
 
-	//Returning default path for saving screenshot
+
+//Returning default path for saving screenshot
 	UFUNCTION(BlueprintPure, meta = (Category = "ScreenshotTaker Functionality"))
-		static FString GetScreenshotSavePath();
+	static FString GetScreenshotSavePath();
+
+
+};
+UCLASS()
+class UAsyncScreenshotRTAction : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext = "WorldContextObject"), Category = "ScreenshotTaker Functionality")
+	static UAsyncScreenshotRTAction* SaveRenderTarget(UObject* WorldContextObject, UTextureRenderTarget2D* RenderTarget, FString PathToSave, FString Name, bool bFlushRHI);
+
+	// UBlueprintAsyncActionBase interface
+	virtual void Activate() override;
+	//~UBlueprintAsyncActionBase interface
+
+	UPROPERTY()
+	UObject* WorldContextObject;
+
+	UPROPERTY()
+	UTextureRenderTarget2D* RT;
+
+	int32 X;
+	int32 Y;
+	bool bFlushRHI;
+
+	TSharedPtr<FAsyncReadEntireRTData, ESPMode::ThreadSafe> ReadRTData;
+
+	UPROPERTY(BlueprintAssignable)
+	FAsyncReadEntireRTOutputPin OnSaveRenderTarget;
+
+	FString SavedPathToSave;
+	FString SavedName;
+
+protected:
+	UFUNCTION()
+	void OnNextFrame();
+
+	uint64 StartFrame;
 };
